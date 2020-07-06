55b24107e938a854c73b189f0b7feaa905cd5596
MonettiSa <MonettiSa@VT-MonettiSa.valueteam.com>
2020-07-06 15:29:36 +0200

    added a second project to repository

---
+++ Esercizio2/src/com/nttdata/TestEsercizio2.java
@@ -0,0 +1,15 @@
+package com.nttdata;
+
+public class TestEsercizio2 {
+
+	public static void main(String[] args) {
+		int n=0;
+		
+		while (n<10) {
+			System.out.println(n+1);
+			n++;
+		}
+
+	}
+
+}
